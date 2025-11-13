9fb8dbc705491d559c044f58cc8c4d6339b8d1fe
Parth Vaghasiya <parthvaghasiya.kalathiyainfo@gmail.com>
2025-11-13 16:31:10 +0530

    Parth13-11

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': "320px",
+      'sm': "425px",
+      'md600': "600px",
+      'md': "768px",
+      'lg': "1024px",
+      'xl': "1280px",
+      "2xl": "1440px",
+      "3xl": "1650px",
+      "4xl": "1920px",
+      "5xl": "2560px",
+    },
     extend: {},
   },
   plugins: [],
