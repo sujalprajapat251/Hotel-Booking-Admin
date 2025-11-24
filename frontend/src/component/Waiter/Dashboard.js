2eb7e075cffc0bb772df362760045da0c0788087
Feniltogadiya <feniltogadiya4@gmail.com>
2025-11-24 11:37:32 +0530

    fenil24-11

--- frontend/src/component/Waiter/Dashboard.js
+++ frontend/src/component/Waiter/Dashboard.js
@@ -1,41 +1,9 @@
-import React, { useEffect } from 'react'
-import tableImg from '../../Images/table.png'
-import { useState } from 'react'
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllCafeTable } from '../../Redux/Slice/cafeTable.slice';
-import { Link } from 'react-router-dom';
-export default function Dashboard() {
-  const dispatch = useDispatch();
+import React from 'react'
 
-    const getCafeTableData = useSelector((state) => state.cafeTable.cafeTable);
-    useEffect(() => {
-        dispatch(getAllCafeTable());
-      }, [dispatch]);
-    return (
-        <div className="p-4 md:p-6 bg-[#f0f3fb] h-full">
-            <div className="mb-6">
-                <h1 className="text-2xl md:text-3xl font-bold text-gray-800">Order / Table Management</h1>
-            </div>
-            <div className=" p-4 flex flex-wrap">
-                {getCafeTableData.map((item, idx) => (
-                    <Link to={'/waiter/table/'+item._id} className='p-3 w-1/6 '>
-                        <div key={idx} className=" p-8 bg-white rounded-md hover:shadow-xl cursor-pointer relative">
-                            <img src={tableImg} alt="table" className="w-24 mx-auto" />
-                            <div className='flex justify-between items-center '>
-                                <h3>{item.title}</h3>
-                                <p>member : {item.limit}</p>
-                            </div>
-                            <div className='absolute right-4 top-4'>
-                                {item.status === true ?
-                                    <p className='bg-green-400 px-2 text-xs rounded-[4px]'>Avaible</p> :
-                                    <p className='bg-red-500 px-2 text-xs  rounded-[4px]'>occupide</p>
-                                }
-                            </div>
-                        </div>
-                    </Link>
-                ))}
-            </div>
-        </div>
-    )
+export default function Dashboard() {
+  return (
+    <div className=''>
+      Dashboard  
+    </div>
+  )
 }
--- frontend/src/component/Waiter/Table.js
+++ frontend/src/component/Waiter/Table.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import tableImg from '../../Images/table.png'
+import { useState } from 'react'
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllCafeTable } from '../../Redux/Slice/cafeTable.slice';
+import { Link } from 'react-router-dom';
+export default function Dashboard() {
+  const dispatch = useDispatch();
 
-export default function Table() {
-  return (
-    <div className=''>
-      table management 
-    </div>
-  )
+    const getCafeTableData = useSelector((state) => state.cafeTable.cafeTable);
+    useEffect(() => {
+        dispatch(getAllCafeTable());
+      }, [dispatch]);
+    return (
+        <div className="p-4 md:p-6 bg-[#f0f3fb] h-full">
+            <div className="mb-6">
+                <h1 className="text-xl md:text-3xl font-bold text-gray-800">Order / Table Management</h1>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4 p-3 md:p-4">
+                {getCafeTableData.map((item, idx) => (
+                    <Link to={'/waiter/table/'+item._id} className='block' key={item._id || idx}>
+                        <div className="p-8 sm:p-10 bg-white rounded-md hover:shadow-xl cursor-pointer relative">
+                            <img src={tableImg} alt="table" className="w-24 mx-auto" />
+                            <p className='text-center mt-3 text-sm sm:text-base'>
+                                {item.title} &nbsp; member: {item.limit}
+                            </p>
+                            <div className='absolute right-3 top-4'>
+                                {item.status === true ?
+                                    <p className='bg-green-400 px-2 py-0.5 text-xs rounded-[4px]'>Available</p> :
+                                    <p className='bg-red-500 px-2 py-0.5 text-xs rounded-[4px]'>Occupied</p>
+                                }
+                            </div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
 }
