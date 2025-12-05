c8724442c250952eeeb70244221dbc29436d0b43
Parth Vaghasiya <parthvaghasiya.kalathiyainfo@gmail.com>
2025-12-05 16:44:21 +0530

    Parthbarchart

--- frontend/src/component/HOD/HODTotalOrderchart.jsx
+++ frontend/src/component/HOD/HODTotalOrderchart.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { AreaChart, Area, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, Tooltip, ResponsiveContainer, Bar, BarChart, Cell } from 'recharts';
 
 const CustomTooltip = ({ active, payload }) => {
   if (active && payload && payload.length) {
@@ -20,11 +20,31 @@ const CustomTooltip = ({ active, payload }) => {
   return null;
 };
 
+const warmColors = ["#F7DF9C", "#E3C78A", "#B79982", "#A3876A", "#876B56", "#755647"];
+
+
+const getPath = (x, y, width, height) => {
+  return `M${x},${y + height}
+    C${x + width / 3},${y + height}
+     ${x + width / 2},${y + height / 3}
+     ${x + width / 2},${y}
+    C${x + width / 2},${y + height / 3}
+     ${x + (2 * width) / 3},${y + height}
+     ${x + width},${y + height}
+    Z`;
+};
+
+const TriangleBar = (props) => {
+  const { fill, x, y, width, height } = props;
+  return <path d={getPath(Number(x), Number(y), Number(width), Number(height))} fill={fill} />;
+};
+
 export default function Example() {
 
   const getDashboardData = useSelector(
     (state) => state.HODDashboard.getHodDashboard
   );
+  console.log('getDashboardData', getDashboardData);
 
   const chartData = getDashboardData?.orderSources
     ? Object.entries(getDashboardData.orderSources).map(([key, value]) => ({
@@ -63,5 +83,28 @@ export default function Example() {
         </AreaChart>
       </ResponsiveContainer>
     </div>
+
+    // <div style={{ width: '100%', maxWidth: '220px', height: '120px' }}>
+    //   <ResponsiveContainer width="100%" height="100%">
+    //     <BarChart
+    //       data={chartData}
+    //       margin={{ top: 10, right: 10, left: 10, bottom: 10 }}
+    //     >
+    //       <Tooltip content={<CustomTooltip />} />
+
+    //       <Bar
+    //         dataKey="value"
+    //         shape={<TriangleBar />}
+    //       >
+    //         {chartData.map((_, index) => (
+    //           <Cell
+    //             key={`cell-${index}`}
+    //             fill={warmColors[index % warmColors.length]}
+    //           />
+    //         ))}
+    //       </Bar>
+    //     </BarChart>
+    //   </ResponsiveContainer>
+    // </div>
   );
 }
